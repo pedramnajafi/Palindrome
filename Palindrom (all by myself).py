c69dc5c2afb7ee91dc55426e0dfb04b35998cb96
Pedram Najafizadeh <82384259+pedramnajafi@users.noreply.github.com>
2022-12-28 19:45:28 +0330

    Add files via upload

---
+++ Palindrom (all by myself).py
@@ -0,0 +1,25 @@
+run = True
+while run:
+    user_input = input('Please type a word or number to check if it is a Palindrom or not or type exit: ')
+
+    if user_input == 'exit':
+        run = False
+        break
+
+    user = ''.join(filter(str.isalnum, user_input)) # removes all the non-alphanumic characters.
+    lowercase = user.lower() # turn the input word into a lowercase word - dows nothing on numbers.
+    characters = list(lowercase) # turn the input word into a list of characters.
+
+    length = len(characters) # counts the number of characters
+
+
+    for c,i in zip(characters, range(length)):
+        reverse_char = characters[(length - 1 - i)]
+        if c != reverse_char:
+            print(False)
+            break
+        else :
+            if i == length-1:
+                print(True)
+
+
